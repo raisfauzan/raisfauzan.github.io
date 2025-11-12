ac282a3b50d1fdbfdf690fda1a2e9704a81bed7e
raisfauzan <raisfauzan11@gmail.com>
2025-11-12 11:17:56 +0700

    update pekerjaan

--- js/script.js
+++ js/script.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*==================== typed js ====================*/
 const typed = new Typed(".multiple-text", {
-  strings: ["Dosen Pendidikan Matematika", "Web Developer", "Researcher"],
+  strings: ["Lecturer", "Web Developer", "Stock Trader"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
