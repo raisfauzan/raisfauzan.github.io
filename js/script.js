dc7533c4670fa7bf415492fae326f56bbc69ea73
raisfauzan <raisfauzan11@gmail.com>
2025-11-12 10:58:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+  /*==================== sticky navbar ====================*/
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+  //reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portofolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+/*==================== typed js ====================*/
+const typed = new Typed(".multiple-text", {
+  strings: ["Dosen Pendidikan Matematika", "Web Developer", "Researcher"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+// Card Slider
+//Ambil elemen wrapper cards
+const wrapper = document.getElementById("cardsWrapper");
+
+//Definisikan jarak geser(misalnya, geser sebanyak lebar 1 card penuh)
+function scrollCards(direction) {
+  //menghitung lebar 1 card (termasuk margin)
+  const card = wrapper.querySelector('.services-box');
+  if(!card) return;
+
+  // lebar card + margin kanan
+  const cardWidth = card.offsetWidth + 20;
+
+  // Hitung posisi geser baru
+  // direction = 1 untuk geser kanan, -1 untuk geser kiri
+  wrapper.scrollLeft += direction * cardWidth;
+}
+
